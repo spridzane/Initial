c831bfba2463f849df3c82c22981f838376d07a9
spridzane <zane.deksne@gmail.com>
2020-06-10 23:39:03 +0300

    Activity10 update

--- src/main/java/jtm/activity10/StreamEditor.java
+++ src/main/java/jtm/activity10/StreamEditor.java
@@ -51,7 +51,7 @@ public class StreamEditor {
 		String curLineContent; // Content of current line of input file
 		boolean delete = false; // Should delete line?
 
-		/*- TODO Check number of passed parameters. If they are null or number of
+		/*-Check number of passed parameters. If they are null or number of
 		 * them is not 4, write to standard error (System.err):
 		 * Please use arguments: [-]lineNo (TextToAdd/Replace|-) (inputFile|-) (outputFile|-)
 		 * and exit with System.exit(1); to pass error status of finished program.
@@ -66,17 +66,16 @@ public class StreamEditor {
 		// deleted if number is negative.
 		// Hint. Use Integer.parseInt() to parse String into integer
 		inLineNo = Integer.parseInt(args[0]);
-		
-		if(inLineNo < 0) {
+
+		if (inLineNo < 0) {
 			delete = true;
-		}else {
-			delete = false;
+			inLineNo = Math.abs(inLineNo);
 		}
 
-		// TODO set value of the string from 1st parameter into content
+		// set value of the string from 1st parameter into content
 		content = args[1];
 
-		/*- TODO Initialize new buffered character reader (BufferedReader) and:
+		/*- Initialize new buffered character reader (BufferedReader) and:
 		 * 1. If input file name (3rd parameter) is "-", add reader to the Standard input (System.in).
 		 * 2. Otherwise check if file exists (if it doesn't, create it) and 
 		 *    add reader to this file.
@@ -95,7 +94,7 @@ public class StreamEditor {
 			reader = new BufferedReader(new FileReader(inFile));
 		}
 
-		/*- TODO Initialize new buffered character writer (PrintWriter) and:
+		/*- Initialize new buffered character writer (PrintWriter) and:
 		 *  1. If output file name (4th parameter) is "-", add writer to the standard output (System.out)
 		 *  2. Otherwise initialize writer to the file of given name.
 		 */
@@ -107,31 +106,39 @@ public class StreamEditor {
 			writer = new PrintWriter(new FileWriter(outFileName));
 		}
 
-		// TODO Read lines in loop from passed file/standard input till to the
+		// Read lines in loop from passed file/standard input till to the
 		// end. Count number of read lines. Before appending line into writer
 		// check, if it needs to be changed/deleted. Change its value to passed
 		// content or just skip appending it to the writer.
 		// NOTE: append break at the end of written line only if it is NOT null
 		// or empty string!
-		
-		curLineContent = reader.readLine();
-//		while(curLineContent != null) {
-//			curLineNo++;
-//
-//			}
-				
-			
 
-	
-		
-		// TODO If number of input line is larger than number of lines in file,
+		while ((curLineContent = reader.readLine()) != null && curLineContent.length() != 0) {
+			curLineNo++;
+			if (curLineNo == inLineNo) {
+				if (delete) {
+					continue;
+				} else {
+					writer.println(content);
+				}
+			} else {
+				writer.println(curLineContent);
+			}
+
+		}
+
+		// If number of input line is larger than number of lines in file,
 		// pad file with empty lines before necessary line.
-		
-//		while (curLineNo < inLineNo) {
-//
-//		}
-		
-		// TODO flush cache of the writer and close connections of the reader
+
+		if (curLineNo < inLineNo) {
+			while (curLineNo < inLineNo - 1) {
+				curLineNo++;
+				writer.println();
+			}
+			writer.println(content);
+		}
+
+		// flush cache of the writer and close connections of the reader
 		// and writer
 		reader.close();
 		writer.flush();
