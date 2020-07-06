23e4974e3a03b4fc27e9dfe77583bc4d25903bd6
spridzane <zane.deksne@gmail.com>
2020-07-06 16:42:53 +0300

    Activity17 commit

--- src/main/java/jtm/activity17/JettyApplication.java
+++ src/main/java/jtm/activity17/JettyApplication.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.Configuration;
 
 // TODO uncomment these annotations to notify to SpringBoot framework
 // that this class can be run as SpringBoot application
-// @Configuration
-// @EnableAutoConfiguration
-// @ComponentScan
+ @Configuration
+ @EnableAutoConfiguration
+ @ComponentScan
 public class JettyApplication {
 	public static void main(String[] args) throws Exception {
 		// TODO uncomment next line to start JettyApplication web app:
-		// SpringApplication.run(JettyApplication.class, args);
+		SpringApplication.run(JettyApplication.class, args);
 		// Look at
 		// http://docs.spring.io/spring-boot/docs/current/api/org/springframework/boot/SpringApplication.html
 		// for more information.
--- src/main/java/jtm/activity17/JettyController.java
+++ src/main/java/jtm/activity17/JettyController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -30,7 +31,7 @@ public class JettyController {
 	 * @return string as HTML response to the request using UTF-8 encoding for
 	 *         non-Latin characters.
 	 */
-	@GetMapping("")
+	@RequestMapping(value = "/", produces = "text/html;charset=UTF-8", method = RequestMethod.GET)
 	@ResponseBody
 	// This method should work without declared name parameter, request and
 	// response objects,
@@ -48,17 +49,119 @@ public class JettyController {
 	}
 
 	// TODO Implement insertTeacher() method
-	public String insertTeacher() {
-		return "";
+	// RMCOMM public String insertTeacher() {
+	// START
+	@RequestMapping(value = "/insertTeacher", produces = "text/html;charset=UTF-8", method = RequestMethod.GET)
+	@ResponseBody
+	// TODO Implement insertTeacher() method
+	public String insertTeacher(@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "surname", required = false) String surname, HttpServletRequest request,
+			HttpServletResponse response) {
+
+		Boolean status = false;
+		if (name == null || surname == null)
+			return "<form action=''>\nName: <input type='text' name='name' value=''><br/>\n"
+					+ "Surname:<input type='text' name='surname' value=''><br/>\n"
+					+ "<input type='submit' value='Insert'></form><br/>\n<a href='/'>Back</a>\n";
+		try {
+			if (!name.equals("") && !surname.equals("")) {
+				manager = new TeacherManager();
+				status = manager.insertTeacher(name, surname);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (manager != null)
+				manager.closeConnecion();
+		}
+
+		if (status)
+			response.setStatus(HttpServletResponse.SC_OK);
+		else
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+		return status.toString() + "<br/>\n" + "<a href='/'>Back</a>\n";
+		// END
+		// RMCOMM return "";
 	}
 
 	// TODO Implement findTeacher() method
-	public String findTeacher() {
-		return "";
+	// RMCOMM public String findTeacher() {
+	// START
+	@RequestMapping(value = "/findTeacher", produces = "text/html;charset=UTF-8", method = RequestMethod.GET)
+	@ResponseBody
+	public String findTeacher(@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "surname", required = false) String surname) {
+
+		List<Teacher> teachers = new ArrayList<Teacher>();
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<form action=''>\n");
+		sb.append("Name: <input type='text' name='name' value=''><br/>\n");
+		sb.append("Surname:<input type='text' name='surname' value=''><br/>\n");
+		sb.append("<input type='submit' value='Find'><br/>\n");
+
+		if (name != null && surname != null) {
+			manager = new TeacherManager();
+			sb.append("<table>\n");
+			try {
+				teachers = manager.findTeacher(name, surname);
+				for (Teacher teacher : teachers) {
+					sb.append("<tr>\n");
+					sb.append("<td>" + teacher.getId() + "</td>\n");
+					sb.append("<td>" + teacher.getFirstName() + "</td>\n");
+					sb.append("<td>" + teacher.getLastName() + "</td>\n");
+					sb.append("</tr>\n");
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			finally {
+				manager.closeConnecion();
+			}
+			sb.append("</table><br>\n");
+		}
+
+		sb.append("<a href='/'>Back</a>\n");
+		return sb.toString();
+		// END
+		// RMCOMM return "";
 	}
 
 	// TODO Implement deleteTeacher() method
-	public String deleteTeacher() {
-		return "";
+	// RMCOMM public String deleteTeacher() {
+	// START
+	@RequestMapping(value = "/deleteTeacher", produces = "text/html;charset=UTF-8", method = RequestMethod.GET)
+	@ResponseBody
+	public String deleteTeacher(@RequestParam(value = "id", required = false) String id, HttpServletRequest request,
+			HttpServletResponse response) {
+		Boolean status = false;
+		if (id == null)
+			return "" + //
+					"<form action=''>\n" + //
+					"ID:<input type='text' name='id' value=''><br/>\n" + //
+					"<input type='submit' value='Delete'><br/>\n" + //
+					"<a href='/'>Back</a>\n";
+		try {
+			if (!"".equals(id)) {
+				manager = new TeacherManager();
+				status = manager.deleteTeacher(Integer.parseInt(id));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (manager != null)
+				manager.closeConnecion();
+		}
+
+		if (status)
+			response.setStatus(HttpServletResponse.SC_OK);
+		else
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		return status.toString() + "<br/>\n<a href='/'>Back</a>\n";
+		// END
+		// RMCOMM return "";
 	}
 }
